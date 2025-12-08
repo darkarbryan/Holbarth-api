fc5ea9080e805250c22c865df867d2b5e8f375e5
Bryan valderrama <bryan_del_95@hotmail.com>
2025-12-08 12:48:21 -0500

    @feature: implement JWT & passport to protect endpoints by API controllers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/core/entities';
@@ -5,17 +7,30 @@ import { LoginRepository } from './infraestructure';
 import { LoginPort } from './domain/ports';
 import { LogInService } from './application/use-cases/log-in';
 import { AuthController } from './application/controllers';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity])],
-    controllers: [AuthController],
-    providers: [
-        {
-            provide: LoginPort,
-            useClass: LoginRepository,
-        },
-        LogInService,
-    ],
-    exports: [LoginPort],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: `${process.env.JWT_SECRET_KEY ?? 'Holbarth2025*'}`,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    {
+      provide: LoginPort,
+      useClass: LoginRepository,
+    },
+    LogInService,
+    JwtStrategy,
+  ],
+  exports: [LoginPort, PassportModule, JwtModule],
 })
 export class AuthModule {}
---
+++ src/modules/auth/jwt-auth.guard.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
